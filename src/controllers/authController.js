ed8931a102ba72874c31114f0656ca6f94a20789
Babaraks <reyes.reser23@gmail.com>
2025-03-11 19:57:26 -0600

    Microservicio.2

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,30 +5,49 @@ const { getCashierByEmail } = require('../models/cashierModel');
 require('dotenv').config();
 
 const login = async (req, res) => {
-    const { email, password} = req.body;
+    const { email, password } = req.body;
+    
     try {
         let user;
-        let userType = 'cajero';
+        let userType = null; // Cambiamos el tipo por defecto a `null`
 
         user = await getUserByEmail(email);
         if (user) {
-            userType = user.rol;
-        }else{
+            userType = user.rol; // Si es un usuario normal, asignamos su rol
+        } else {
             user = await getCashierByEmail(email);
+            if (user) {
+                userType = "cajero"; // Si es cajero, lo marcamos como "cajero"
+            }
         }
+
         if (!user) {
             return res.status(401).json({ message: 'Usuario no encontrado' });
         }
-        const isMarch = await bcrypt.compare(password, user.password);
-        if (!isMarch) {
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
             return res.status(401).json({ message: 'Contraseña incorrecta' });
         }
-        const token = jwt.sign({ id: user.id, email: user.email, rol: userType }, process.env.SECRET, {
-            expiresIn: 86400,
+
+        // Generamos el token con la información del usuario
+        const token = jwt.sign(
+            { id: user.id, email: user.email, rol: userType },
+            process.env.SECRET,
+            { expiresIn: 86400 }
+        );
+
+        // Ahora devolvemos el token junto con los datos del usuario
+        res.json({
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                rol: userType, // Incluimos el rol en la respuesta
+            }
         });
-        res.json({ token });
-    }
-    catch (error) {
+
+    } catch (error) {
         res.status(500).json({ message: 'Error al iniciar sesión', error: error.message });
     }
 }

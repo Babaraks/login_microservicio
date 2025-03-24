54efe30fe4c51ce64d8349853a36a3912a3cd2ac
Babaraks <reyes.reser23@gmail.com>
2025-03-24 08:30:08 -0600

    sin-cajero

--- src/server.js
+++ src/server.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const authRoutes = require('./routes/authRoutes');
-const cashierRoutes = require('./routes/cashierRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();

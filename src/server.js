2a46053862ea13d1dabdd5811dd3b932cebfd028
Babaraks <reyes.reser23@gmail.com>
2025-04-01 08:27:00 -0600

    funciona

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const userRoutes = require('./src/routes/userRoutes');
+const userRoutes = require('./routes/userRoutes');
 
-dotenv.config();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
 const app = express();
-app.use(express.json()); // Middleware para parsear JSON
+app.use(cors());
+app.use(bodyParser.json());
 
-// Rutas de usuario
+
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;

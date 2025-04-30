95c686993c5478610dd1e7b3bd8a0706b99acc07
Babaraks <reyes.reser23@gmail.com>
2025-04-30 13:18:29 -0600

    cierre conexion

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,38 +1,64 @@
 const pool = require('../config/db');
 
 const getAllProducts = async () => {
-    const result = await pool.query('SELECT * FROM public.producto ORDER BY "ID" ASC');
-    return result.rows;
-}
+    const client = await pool.connect();
+    try {
+        const result = await client.query('SELECT * FROM public.producto ORDER BY "ID" ASC');
+        return result.rows;
+    } finally {
+        client.release();
+    }
+};
+
 const getProductById = async (id) => {
-    const result = await pool.query(
-        'SELECT * FROM public.producto WHERE "ID" = $1', 
-        [id]
-    );
-    return result.rows;
-}
+    const client = await pool.connect();
+    try {
+        const result = await client.query(
+            'SELECT * FROM public.producto WHERE "ID" = $1', 
+            [id]
+        );
+        return result.rows;
+    } finally {
+        client.release();
+    }
+};
 
 const createProduct = async (product) => {
     const { nombre, precio, tipo } = product;
-    const result = await pool.query(
-        'INSERT INTO producto ("nombre", "tipo", "precio") VALUES ($1, $2, $3) RETURNING *',
-        [nombre, tipo,precio ]
-    );
-    return result.rows[0];
-}
+    const client = await pool.connect();
+    try {
+        const result = await client.query(
+            'INSERT INTO producto ("nombre", "tipo", "precio") VALUES ($1, $2, $3) RETURNING *',
+            [nombre, tipo, precio]
+        );
+        return result.rows[0];
+    } finally {
+        client.release();
+    }
+};
 
 const updateProduct = async (id, nombre, tipo, precio) => {
-    const result = await pool.query(
-        'UPDATE producto SET "nombre" = $1, "tipo" = $2, "precio" = $3 WHERE ID = $4 RETURNING *',
-        [nombre, tipo, precio, id]
-    );
-    return result.rows[0];
-}
-const deleteProduct = async (id) => {
-    await pool.query('DELETE FROM producto WHERE "ID" = $1', [id]);
-    return { message: 'Producto eliminado' };
-}
+    const client = await pool.connect();
+    try {
+        const result = await client.query(
+            'UPDATE producto SET "nombre" = $1, "tipo" = $2, "precio" = $3 WHERE ID = $4 RETURNING *',
+            [nombre, tipo, precio, id]
+        );
+        return result.rows[0];
+    } finally {
+        client.release();
+    }
+};
 
+const deleteProduct = async (id) => {
+    const client = await pool.connect();
+    try {
+        await client.query('DELETE FROM producto WHERE "ID" = $1', [id]);
+        return { message: 'Producto eliminado' };
+    } finally {
+        client.release();
+    }
+};
 module.exports = {
     getAllProducts,
     getProductById,

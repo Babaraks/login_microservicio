f9ba24a1880375dd3cd312f4cc5c267c9ff487aa
Babaraks <reyes.reser23@gmail.com>
2025-04-30 13:19:54 -0600

    cierre conexion

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,25 +1,47 @@
 const pool = require('../config/db');
 
 const getUserByEmail = async (email) => {
-    const query = `SELECT * FROM public.usuarios WHERE correo = $1`;
-    const values = [email];
-    const result = await pool.query(query, values);
-    return result.rows[0];
-}
+    const client = await pool.connect();
+    try {
+        const query = `SELECT * FROM public.usuarios WHERE correo = $1`;
+        const values = [email];
+        const result = await client.query(query, values);
+        return result.rows[0];
+    } finally {
+        client.release();
+    }
+};
 
-const getMangerById = async (email) => {
-    const query = `SELECT p.*, s."ID" AS ID_punto FROM usuarios p JOIN punto_venta s ON p."ID" = s."ID_encargado" OR p."ID" = s."ID_repartidor" WHERE correo = $1`;
-    const values = [email];
-    const result = await pool.query(query, values);
-    return result.rows[0];
-}
+const getManagerById = async (email) => {
+    const client = await pool.connect();
+    try {
+        const query = `SELECT p.*, s."ID" AS ID_punto 
+                       FROM usuarios p 
+                       JOIN punto_venta s ON p."ID" = s."ID_encargado" OR p."ID" = s."ID_repartidor" 
+                       WHERE correo = $1`;
+        const values = [email];
+        const result = await client.query(query, values);
+        return result.rows[0];
+    } finally {
+        client.release();
+    }
+};
 
 const getCashierById = async (email) => {
-    const query = `SELECT p.*, s."ID" AS ID_punto FROM usuarios p JOIN punto_cajero s ON p."ID" = s."ID_cajero" WHERE correo = $1`;
-    const values = [email];
-    const result = await pool.query(query, values);
-    return result.rows[0];
-}
+    const client = await pool.connect();
+    try {
+        const query = `SELECT p.*, s."ID" AS ID_punto 
+                       FROM usuarios p 
+                       JOIN punto_cajero s ON p."ID" = s."ID_cajero" 
+                       WHERE correo = $1`;
+        const values = [email];
+        const result = await client.query(query, values);
+        return result.rows[0];
+    } finally {
+        client.release();
+    }
+};
+
 
 module.exports = {
     getUserByEmail,
